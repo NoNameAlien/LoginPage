739338616869a43650b330bf84554977aee97913
Thomasina Katherin <thomasik@ge-e5.nsk.21-school.ru>
2023-10-22 18:42:41 +0700

    newbuild

--- src/Controllers/src/Controllers/LoginController.cpp
+++ src/Controllers/src/Controllers/LoginController.cpp
@@ -1,11 +1,11 @@
-#include "LoginController.h"
+//#include "LoginController.h"
 
-LoginController::LoginController(QObject* parent) : QObject(parent) {
-    // Соединяем сигнал из AuthenticationService с loginResult сигналом LoginController
-    connect(&authenticationService, &AuthenticationService::authenticationResult, this, &LoginController::loginResult);
-}
+//LoginController::LoginController(QObject* parent) : QObject(parent) {
+//    // Соединяем сигнал из AuthenticationService с loginResult сигналом LoginController
+//    connect(&authenticationService, &AuthenticationService::authenticationResult, this, &LoginController::loginResult);
+//}
 
-void LoginController::login(const QString& username, const QString& password) {
-    // Передаем данные аутентификации в AuthenticationService
-    authenticationService.authenticate(username, password);
-}
+//void LoginController::login(const QString& username, const QString& password) {
+//    // Передаем данные аутентификации в AuthenticationService
+//    authenticationService.authenticate(username, password);
+//}
